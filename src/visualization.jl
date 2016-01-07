b730b678c8f5c4d1d4f99093d210b51612a4ca47
C HO <christopherj.ho@gmail.com>
2016-01-06 18:37:16 -0800

    more stuff

--- src/visualization.jl
+++ src/visualization.jl
@@ -6,16 +6,43 @@ using PyPlot #make my life easy
 draw_box(x::Union{Float64,Int},y::Union{Float64,Int},w::Union{Float64,Int},h::Union{Float64,Int})=fill([x;x+w;x+w;x],[y;y;y+h;y+h])
 draw_box(x::Union{Float64,Int},y::Union{Float64,Int},w::Union{Float64,Int},h::Union{Float64,Int},color::AbstractString)=fill([x;x+w;x+w;x],[y;y;y+h;y+h],color=color)
 
+function draw_sedan(pomdp::MLPOMDP,x_ctr::Float64,y_ctr::Float64,color::AbstractString)
+	#x_ctr = pomdp.POSITIONS[s.pos[1]]
+	#y_ctr = pomdp.phys_param.y_interval*s.pos[2]
+	w = pomdp.phys_param.l_car
+	h = pomdp.phys_param.w_car
+	x = x_ctr - 0.5*w
+	y = y_ctr - 0.5*h
+
+	##TODO: color stuff based on behavior Model
+	draw_box(x,y,w,h,color)
+	wind_color = ""
+	x_windshield = x_ctr
+	w_windshield = w*0.25
+	h_windshield = h*0.75
+	y_windshield = y_ctr - 0.5*h_windshield
+	draw_box(x_windshield,y_windshield,w_windshield,h_windshield,window_color)
+	x_backwindow = x_ctr - 0.25*w
+	w_backwindow = w*0.25
+	h_backwindow = w*0.75
+	y_backwindow = y_ctr - *0.5*h_backwindow
+	draw_box(x_backwindow,y_backwindow,w_backwindow,h_backwindow,window_color)
+	##TODO: add direction arrow
+	##TODO: add a face based on behavior model
+end
+
 function draw_sedan(pomdp::MLPOMDP,s::CarState)
 	x_ctr = pomdp.POSITIONS[s.pos[1]]
 	y_ctr = pomdp.phys_param.y_interval*s.pos[2]
-	x = x_ctr - 0.5*pomdp.phys_param.l_car
-	y = y_ctr - 0.5*pomdp.phys_param.w_car
+	##TODO: something to do with behavior and color
+	color = "" #PLACEHOLDER
+	draw_sedan(pomdp,x_ctr,y_ctr,color)
+	#draw_direction?
+	#draw_ face?
+end
 
-	##TODO: color stuff based on behavior Model
-	draw_box(x,y,pomdp.phys_param.l_car,pomdp.phys_param.w_car)
-	##TODO: add direction arrow
-	##TODO: add a face based on behavior model
+function draw_crash(pomdp::MLPOMDP,s::MLState,a::MLAction)
+
 end
 
 function visualize(pomdp::MLPomdp,s::MLState,a::MLAction)
@@ -51,6 +78,10 @@ function visualize(pomdp::MLPomdp,s::MLState,a::MLAction)
 	end
 
 	#draw self car
+	x_ctr = W/2.
+	y_ctr = pomdp.phys_param.y_interval*s.pos
+	color = "" #PLACEHOLDER
+	draw_sedan(pomdp,x_ctr,y_ctr,color)
 
 	#draw environment cars
 	for car in s.env_cars
@@ -58,5 +89,8 @@ function visualize(pomdp::MLPomdp,s::MLState,a::MLAction)
 	end
 
 	#if is_crash, add crash graphic
+	if is_crash(pomdp,s,a)
+		draw_crash(pomdp,s,a)
+	end
 
 end
