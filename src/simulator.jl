c37d3b4078fb3f4c0c3bb28a8b79dae12f7e1bf9
Zachary Sunberg <sunbergzach@gmail.com>
2016-04-09 13:28:29 -0700

    notebook works

--- src/simulator.jl
+++ src/simulator.jl
@@ -29,7 +29,7 @@ function rand(rng::AbstractRNG,action_space::ActionSpace,::Union{MLState,MLActio
   return domain(action_space)[r]
 end
 
-GenerativeModels.generate_s(pomdp::MLPOMDP, s::MLState, a::MLAction, rng::AbstractRNG, s::MLState=create_state(pomdp)) = next(rng, pomdp, s, a)
+GenerativeModels.generate_s(pomdp::MLPOMDP, s::MLState, a::MLAction, rng::AbstractRNG, sp::MLState=create_state(pomdp)) = next(rng, pomdp, s, a)
 
 function next(rng::AbstractRNG,pomdp::MLPOMDP,s::MLState,a::MLAction)
   dt = pomdp.phys_param.dt
